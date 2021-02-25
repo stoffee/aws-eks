cfdaac7d87232d99d7f5320bd67366a97298b372
stoffee <chris@roadkill.org>
2021-02-25 13:43:03 -0800

    changes

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ module "subnets" {
 
 
 module "eks_cluster" {
-  source = "../../"
+  source = "cloudposse/eks-cluster/aws"
 
   region                       = var.region
   vpc_id                       = module.vpc.vpc_id
