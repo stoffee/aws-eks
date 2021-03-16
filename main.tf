fc4bce694da877759140f96f776f4b9811d1b01b
stoffee <chris@roadkill.org>
2021-03-16 11:15:11 -0700

    new module

--- main.tf
+++ main.tf
@@ -1,105 +1,163 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 0.12.0"
 }
 
-module "label" {
-  source     = "cloudposse/label/null"
-  version    = "0.22.0"
-  attributes = ["cluster"]
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = var.region
+}
 
-  context = module.this.context
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.10"
+}
+
+data "aws_availability_zones" "available" {
 }
 
 locals {
-  # The usage of the specific kubernetes.io/cluster/* resource tags below are required
-  # for EKS and Kubernetes to discover and manage networking resources
-  # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#base-vpc-networking
-  tags = merge(module.label.tags, map("kubernetes.io/cluster/${module.label.id}", "shared"))
+  cluster_name = "test-eks-${random_string.suffix.result}"
+}
 
-  # Unfortunately, most_recent (https://github.com/cloudposse/terraform-aws-eks-workers/blob/34a43c25624a6efb3ba5d2770a601d7cb3c0d391/main.tf#L141)
-  # variable does not work as expected, if you are not going to use custom ami you should
-  # enforce usage of eks_worker_ami_name_filter variable to set the right kubernetes version for EKS workers,
-  # otherwise will be used the first version of Kubernetes supported by AWS (v1.11) for EKS workers but
-  # EKS control plane will use the version specified by kubernetes_version variable.
-  eks_worker_ami_name_filter = "amazon-eks-node-${var.kubernetes_version}*"
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
 
-  # required tags to make ALB ingress work https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html
-  public_subnets_additional_tags = {
-    "kubernetes.io/role/elb" : 1
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
   }
-  private_subnets_additional_tags = {
-    "kubernetes.io/role/internal-elb" : 1
-  }
+}
 
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
 }
 
 module "vpc" {
-  source  = "cloudposse/vpc/aws"
-  version = "0.17.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
 
-  cidr_block = "172.16.0.0/16"
-  tags       = local.tags
+  name                 = "test-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  context = module.this.context
-}
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
 
-module "subnets" {
-  source  = "cloudposse/dynamic-subnets/aws"
-  version = "0.28.0"
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
 
-  availability_zones              = var.availability_zones
-  vpc_id                          = module.vpc.vpc_id
-  igw_id                          = module.vpc.igw_id
-  cidr_block                      = module.vpc.vpc_cidr_block
-  nat_gateway_enabled             = true
-  nat_instance_enabled            = false
-  tags                            = local.tags
-  public_subnets_additional_tags  = local.public_subnets_additional_tags
-  private_subnets_additional_tags = local.private_subnets_additional_tags
-
-  context = module.this.context
-}
-
-
-module "eks_cluster" {
-  source = "cloudposse/eks-cluster/aws"
-
-  region                       = var.region
-  vpc_id                       = module.vpc.vpc_id
-  subnet_ids                   = concat(module.subnets.private_subnet_ids, module.subnets.public_subnet_ids)
-  kubernetes_version           = var.kubernetes_version
-  local_exec_interpreter       = var.local_exec_interpreter
-  oidc_provider_enabled        = var.oidc_provider_enabled
-  enabled_cluster_log_types    = var.enabled_cluster_log_types
-  cluster_log_retention_period = var.cluster_log_retention_period
-
-  context = module.this.context
-}
-
-# Ensure ordering of resource creation to eliminate the race conditions when applying the Kubernetes Auth ConfigMap.
-# Do not create Node Group before the EKS cluster is created and the `aws-auth` Kubernetes ConfigMap is applied.
-# Otherwise, EKS will create the ConfigMap first and add the managed node role ARNs to it,
-# and the kubernetes provider will throw an error that the ConfigMap already exists (because it can't update the map, only create it).
-# If we create the ConfigMap first (to add additional roles/users/accounts), EKS will just update it by adding the managed node role ARNs.
-data "null_data_source" "wait_for_cluster_and_kubernetes_configmap" {
-  inputs = {
-    cluster_name             = module.eks_cluster.eks_cluster_id
-    kubernetes_config_map_id = module.eks_cluster.kubernetes_config_map_id
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
-module "eks_node_group" {
-  source  = "cloudposse/eks-node-group/aws"
-  version = "0.8.0"
+module "eks" {
+  source       = "anubhavmishra/eks/aws"
+  cluster_name = local.cluster_name
+  subnets      = module.vpc.private_subnets
 
-  subnet_ids        = module.subnets.private_subnet_ids
-  cluster_name      = data.null_data_source.wait_for_cluster_and_kubernetes_configmap.outputs["cluster_name"]
-  instance_types    = var.instance_types
-  desired_size      = var.desired_size
-  min_size          = var.min_size
-  max_size          = var.max_size
-  kubernetes_labels = var.kubernetes_labels
-  disk_size         = var.disk_size
+  tags = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
 
-  context = module.this.context
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  map_roles                            = var.map_roles
+  map_users                            = var.map_users
+  map_accounts                         = var.map_accounts
 }
